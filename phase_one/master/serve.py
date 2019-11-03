5dea5b64b302f66bbaa7e5695c6bb561e4e738f7
Chester Enright <amunchet@gmail.com>
2019-11-02 23:49:36 -0500

    Updates to master, including tests

--- phase_one/master/serve.py
+++ phase_one/master/serve.py
@@ -3,10 +3,22 @@
 import flask
 import inspect
 import os
+import json
+import urllib
 
 HOSTS_FOLDER="hosts"
 DATA_FOLDER="data"
+URLS_LIST="../master.txt"
 
+TIMEOUT = 15
+MAX=29
+
+
+# URI Helper function
+def path_safe(name):
+    return urllib.parse.quote(name, safe='')
+
+# Auth function
 def auth(func):
     """Authentication wrapper"""
     def decorator(*args, **kwargs):
@@ -23,28 +35,43 @@ def auth(func):
 @auth
 def exists(auth, ip):
     """Checks if the IP is already registered"""
-    
-    pass
-
+    if os.path.exists(os.path.join(HOSTS_FOLDER, ip)):
+        return 200, "Client exists"
+    else:
+        return 404, "Client not found"
+@auth
 def register(auth, ip):
     """Registers the IP"""
-    pass
-
+    if exists(auth, ip)[0] == 200:
+        return 202, "Client already registered"
+    else:
+        with open(os.path.join(HOSTS_FOLDER, ip), "w") as f:
+            json.dump({
+                    "ip" : ip,
+                    "run_count": 0,
+                    "last_run" : "",
+                    "status" : "new"
+                }, f)
+            return 200, "Client created"
+@auth
 def receive(auth, ip):
     """
     Sends out the allocated URL to receive
         - This is where the server will check how many we've done per hour or day
     """
     pass
-
+@auth
 def status(auth, ip):
     """Callback from the client to let server know we've started our request"""
     pass
-
+@auth
 def post(auth):
-    """Function to receive back the scraped data"""
+    """
+    Function to receive back the scraped data
+        - This might do some weird things with the auth key
+    """
     pass
-
+@auth
 def end(auth, ip):
     """Sends to the server letting us know this host is finished"""
     pass
--- phase_one/master/tests/test_master.py
+++ phase_one/master/tests/test_master.py
@@ -30,7 +30,8 @@ def setup():
     TEMP_HOST = {
             "ip": "192.168.10.100",
             "run_count": 3,
-            "last_run" : str(datetime.now())
+            "last_run" : str(datetime.now()),
+            "status" : "idle"
             }
 
     print ("Deleting all the contents of the test folders")
@@ -48,13 +49,20 @@ def setup():
     
     print ("Creating data file")
     
-    FILENAME = "https://seekingalpha.com"
-    safe_filename = urllib.parse.quote(FILENAME, safe='')
+    FILENAME = "https://facebook.com"
+    safe_filename = serve.path_safe(FILENAME)
 
     with open(os.path.join(DATA_FOLDER, safe_filename), "w") as f:
         f.write("CONTENT FROM THE FILE.\r\nCONTENT FROM THE FILE!")
 
     yield "Setup completed."
+
+    for file in glob.glob(HOSTS_FOLDER + "/**"):
+        os.remove(file)
+
+    for file in glob.glob(DATA_FOLDER + "/**"):
+        os.remove(file)
+
 
     return "Finished clean"
 
@@ -72,6 +80,19 @@ def test_client_exists(setup):
 
 def test_client_register(setup):
     """
+    Tests when a client registers
+        - Will return already registered if alredy exists
+    """
+    a = serve.register(AUTH, "19.168.1.100")
+    assert a != (401, "Unauthorized")
+    assert a == (200, "Client created")
+
+    a = serve.register(AUTH,"192.168.10.100")
+    assert a != (401, "Unauthorized")
+    assert a == (202, "Client already registered")
+
+def test_client_receive(setup):
+    """
     Tests when the client requests the next bite
         - Time elapsed since the previous check (rate limit)
         - Number of bites eaten today
@@ -79,9 +100,58 @@ def test_client_register(setup):
         - End if too many bites - wait if too soon bites
         - Need to update the status (wherver that's happening)
     """
+    # Make sure the data file does not exist
+    serve.URLS_LIST = "test_urls.txt"
+    assert not os.path.exists(os.path.join(DATA_FOLDER, serve.safe_filename("https://seekingalpha.com")))
+
+    a = serve.receive(AUTH, "192.168.10.100")
+    assert a[0] != 401
+    assert a[1] == "https://seekingalpha.com"
+
+    # Make sure we touch the file in data but have no data in it
+
+    assert os.path.exists(os.path.join(DATA_FOLDER, serve.safe_filename("https://seekingalpha.com")))
+
+    old_max = serve.MAX
+    serve.MAX = 1
+
+    a = serve.receive(AUTH, "192.168.10.100")
+    assert a[0] != 401
+    assert a[0] == 429
+    
+    serve.MAX = old_max
+
+    a = serve.receive(AUTH, "192.168.10.100")
+    assert a[0] != 401
+    assert "seekingalpha" not in a[1]
+
+
+def test_client_status(setup):
+    """Client calls, letting us know that they've started the request.  Will just update the Client file"""
     pass
 
-def test_client_retrieve(setup):
-    """Tests retreiving the actual data from the client (i.e. the finished scraped work)"""
+def test_client_status(setup):
+    """
+    """
+    pass
+
+def test_client_post(setup):
+    """
+    Posting back received data
+    """
+    pass
+
+def test_client_end(setup):
+    """
+    Letting server know the client has finished
+        - Basically will just update the client file and that's the end
+    """
+    pass
+
+
+def test_dashboard(setup):
+    """
+    Tests to make sure the dashboard is showing proper things
+    """
     pass
 
