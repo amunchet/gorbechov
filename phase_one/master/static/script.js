f5277317cb56671716de743712ab8db02b19ea1c
Chester Enright <amunchet@gmail.com>
2019-11-05 00:19:47 -0600

    feat(ui): ui updated with proper hiding

--- phase_one/master/static/script.js
+++ phase_one/master/static/script.js
@@ -1,3 +1,54 @@
+/* Custom Javascript for Gorbechov dashboard */
+
+function read_hosts(){
+	$.getJSON("/dashboard_hosts", (data)=>{
+		$("#hosts_table tr.inserted").remove()
+		$.each(data, (key,val)=>{
+			var row_class = ''
+			if (val['run_count'] == 15 || val['run_count'] == 5){
+				row_class = "completed"
+			}
+			$("#hosts_table tr:last").after("<tr class='inserted " + row_class + "'><td>" + val['ip'] + "</td><td>" + val['run_count'] + "</td><td>" + val['last_run'] + "</td><td>" + val['status'] + "</td></tr>")
+		})
+	})
+}
+
+function read_status(){
+	$.getJSON("/dashboard_status", (data) => {
+		$("#data_table tr.inserted").remove()
+		$.each(data, (key,val)=>{
+			var row_class = ''
+			if (val[1].indexOf('data') != -1){
+				row_class = "completed"
+			}
+			$("#data_table tr:last").after("<tr class='inserted " + row_class + "'><td>" + val[0] + "</td><td>" + val[1] + "</td></tr>")
+		})
+	})
+}
+
+function read_count(){
+	$.get("/dashboard_count", data => {
+		$("#total_count").html(data)
+	})
+}
+
+function show_all(){
+	$(".completed").show()
+}
+function hide_all(){
+	$(".completed").hide()
+}
+
 $(document).ready(()=>{
-	console.log("Loaded")
+	
+	read_hosts()
+	read_status()
+	read_count()
+
+
+	setInterval(()=>{
+		read_hosts()
+		read_status()
+		read_count()
+	},30000)
 })
